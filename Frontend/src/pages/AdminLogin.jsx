e24a6ed0e419b75fef46a52c5e47d915a3b43174
Vikas Yadav <vikas1012.in@gmail.com>
2024-07-14 12:29:01 +0530

    FavroitePage Profile Page done

--- Frontend/src/pages/AdminLogin.jsx
+++ Frontend/src/pages/AdminLogin.jsx
@@ -6,6 +6,7 @@ const AdminLogin = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
